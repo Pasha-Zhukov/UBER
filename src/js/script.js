c897c39925634fc427e5fb935785ffe7b89e75d6
Pavel <pasha-zhukov@mail.ru>
2022-01-31 11:26:30 +0300

    added adaptability for different devices

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+    //hamburger script in the mobile version
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
